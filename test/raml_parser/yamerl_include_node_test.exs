cc1b21faa0faad96bb5196701ad7d7ddc12e7344
nat chapman <nat.k.chapman@gmail.com>
2016-03-04 15:49:29 -0600

    changed YIN tests to use ExSpec

--- test/raml_parser/yamerl_include_node_test.exs
+++ test/raml_parser/yamerl_include_node_test.exs
@@ -1,88 +1,112 @@
 defmodule RamlParser.YamerlIncludeNodeTest do
-	use ExUnit.Case
-	alias RamlParser.YamerlIncludeNode, as: YIN
-	require YIN
-	require Record
+  use ExSpec
+  alias RamlParser.YamerlIncludeNode, as: YIN
+  require YIN
+  require Record
 
-	records = Record.extract_all(from_lib: "yamerl/include/yamerl_nodes.hrl")
-	for {name, fields} <- records do
-		Record.defrecordp name, fields
-	end
-	
-	test "simple string" do
-		str = """
-    #%RAML 0.8
-		---
-		prop: !include test
-    """
-		[doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
-		text =
-			doc
-  		|> yamerl_doc(:root)
-			|> yamerl_map(:pairs)
-			|> Enum.at(0)
-			|> elem(1)
-			|> YIN.raml_include(:file)
-		assert text == 'test'
-	end
+  records = Record.extract_all(from_lib: "yamerl/include/yamerl_nodes.hrl")
+  for {name, fields} <- records do
+    Record.defrecordp name, fields
+  end
 
-	test "absolute file name" do
-		str = """
-		#%RAML 0.8
-    ---
-		prop: !include /absolute/path-to/file.yaml
-    """
-    [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
-		text =
-			doc
-  		|> yamerl_doc(:root)
-			|> yamerl_map(:pairs)
-			|> Enum.at(0)
-			|> elem(1)
-			|> YIN.raml_include(:file)
-		assert text == '/absolute/path-to/file.yaml'
-	end
+  describe "Yamerl Include Nodes" do
+    it "should include a simple string" do
+      str = """
+      #%RAML 0.8
+      ---
+      prop: !include test
+      """
+      [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
+      text =
+        doc
+        |> yamerl_doc(:root)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> YIN.raml_include(:file)
 
-	test "relative file name" do
-		str = """
-		#%RAML 0.8
-		---
-		prop: !include ../../relative/file-name.raml
-    """
-		[doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
-		text =
-			doc
-		  |> yamerl_doc(:root)
-			|> yamerl_map(:pairs)
-			|> Enum.at(0)
-			|> elem(1)
-			|> YIN.raml_include(:file)
-		assert text == '../../relative/file-name.raml'
-	end
+      assert text == 'test'
+    end
 
-	test "complicated structure" do
-		str = """
-    #%RAML 0.8
-    ---
-    prop:
-      - a: 0
-        b: 1
-      - c: !include test
-        d: 2
-		"""
-		[doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
-		text =
-			doc
-		  |> yamerl_doc(:root)
-			|> yamerl_map(:pairs)
-			|> Enum.at(0)
-			|> elem(1)
-			|> yamerl_seq(:entries)
-			|> Enum.at(1)
-			|> yamerl_map(:pairs)
-			|> Enum.at(0)
-			|> elem(1)
-			|> YIN.raml_include(:file)
-		assert text == 'test'
-	end
+    it "should include an absolute file name" do
+      str = """
+      #%RAML 0.8
+      ---
+      prop: !include /absolute/path-to/file.yaml
+      """
+      [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
+      text =
+        doc
+        |> yamerl_doc(:root)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> YIN.raml_include(:file)
+
+      assert text == '/absolute/path-to/file.yaml'
+    end
+
+    it "should include a relative file name" do
+      str = """
+      #%RAML 0.8
+      ---
+      prop: !include ../../relative/file-name.raml
+      """
+      [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
+      text =
+        doc
+        |> yamerl_doc(:root)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> YIN.raml_include(:file)
+
+      assert text == '../../relative/file-name.raml'
+    end
+
+    it "should include a uri" do
+      str = """
+      #%RAML 0.8
+      ---
+      prop: !include http://raml.org
+      """
+      [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
+      text =
+        doc
+        |> yamerl_doc(:root)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> YIN.raml_include(:file)
+
+      assert text == 'http://raml.org'
+    end
+
+    it "should correctly include a complicated structure" do
+      str = """
+      #%RAML 0.8
+      ---
+      prop:
+        - a: 0
+          b: 1
+        - c: !include test
+          d: 2
+      """
+      [doc] = :yamerl_constr.string(str, node_mods: [YIN], detailed_constr: true)
+      text =
+        doc
+        |> yamerl_doc(:root)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> yamerl_seq(:entries)
+        |> Enum.at(1)
+        |> yamerl_map(:pairs)
+        |> Enum.at(0)
+        |> elem(1)
+        |> YIN.raml_include(:file)
+
+      assert text == 'test'
+    end
+  end
 end
